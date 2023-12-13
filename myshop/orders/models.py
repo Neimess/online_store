c237e47a43e68f6555bb4b3cc5783aadf96a3730
Neimess <sh0ckerZ7714@gmail.com>
2023-12-14 00:37:45 +0300

    Order, OrderItems models created 14.12.23

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from decimal import Decimal
+from django.core.validators import MinValueValidator
 from shop.models import Product
+from users.models import Profile
 # Create your models here.
+
+
+class Order(models.Model):
+    user = models.OneToOneField(Profile,
+                                on_delete=models.CASCADE)
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE,
+                              null=False)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE,
+                                null=False)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                validators=[
+                                    MinValueValidator(Decimal('0.01'))
+                                ])
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
--- myshop/users/models.py
+++ myshop/users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.urls import reverse
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                null=False)
+    birth_date = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+
+    class Meta:
+        verbose_name = ("profile")
+        verbose_name_plural = ("profiles")
+
+    def __str__(self):
+        return f"User: {self.user}"
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
